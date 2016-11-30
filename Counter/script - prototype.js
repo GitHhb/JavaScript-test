b67fa892836b61df8cb884cec812e3a66bc62c8b
Leandro <githhb@mittere.net>
2016-11-30 02:18:49 +0100

    update prototype with bind()

--- Counter/script - prototype.js
+++ Counter/script - prototype.js
@@ -1,7 +1,10 @@
 var Counter = function (screenObj, min = 10, sec = 0, speed = 1000) {
     // Init vars
     var self = this;
-    var countInterval;
+    this.timer;
+    this.timerSet = false; // bool to indicate whether countInterval is running
+    this.min = min;
+    this.sec = sec;
     this.minCnt = min;
     this.secCnt = sec; 
     this.screenObj = screenObj;
@@ -11,23 +14,22 @@ var Counter = function (screenObj, min = 10, sec = 0, speed = 1000) {
         
 // Define functions
 Counter.prototype.set = function () {
-    this.minCnt = min;
-    this.secCnt = sec;
+    this.minCnt = this.min;
+    this.secCnt = this.sec;
 };
 
 Counter.prototype.display = function () {
     var formatter = new Intl.NumberFormat('en-EN', { minimumIntegerDigits: '2'}).format;
     this.screenObj.innerText = formatter(this.minCnt) + ":" + formatter(this.secCnt);
-    console.log("OK");
 };
         
 Counter.prototype.countDown = function () {
     if (this.minCnt == 0 && this.secCnt == 0) {
-        clearInterval(this.countInterval);
+        clearInterval(this.timer);
         this.display();
     } else if (this.secCnt > 0) {
         this.secCnt--;
-        console.log(this.secCnt, this.x, this.constructor.name, self.constructor.name);
+//        console.log(this.secCnt, this.x, this.constructor.name, self.constructor.name);
         this.display();
         
         return;
@@ -41,40 +43,53 @@ Counter.prototype.countDown = function () {
 
 Counter.prototype.start = function () {
     this.display();
-    countInterval = setInterval(this.countDown, this.speed);
+    if (! this.timerSet) {
+            this.timer = setInterval(this.countDown.bind(this), this.speed);
+            this.timerSet = true;
+    };
 };
 
 Counter.prototype.reset = function () {
-    clearInterval(countInterval);
+    clearInterval(this.timer);
+    this.timerSet = false;
     this.set();
     this.start();
 };
 
 Counter.prototype.stop = function () {
-    clearInterval(countInterval);
+    clearInterval(this.timer);
+    this.timerSet = false;
 }
 
 
 // Init counter 1
 var counter1Elem = document.getElementById("counter1");
 var counter1 = new Counter(counter1Elem, 20, 0);
+// setup reset button
 var counter1But = document.getElementById("counter1-but");
-counter1But.addEventListener("click", counter1.reset);
+counter1But.addEventListener("click", counter1.reset.bind(counter1));
+// setup stop button
 var counter1StopBut = document.getElementById("counter1-stop-but");
-counter1StopBut.addEventListener("click", counter1.stop);
+counter1StopBut.addEventListener("click", counter1.stop.bind(counter1));
+// setup start button
 var counter1StartBut = document.getElementById("counter1-start-but");
-counter1StartBut.addEventListener("click", counter1.start);
+counter1StartBut.addEventListener("click", counter1.start.bind(counter1));
+// Start
 counter1.start();
 
 // Init counter 2
 var counter2Elem = document.getElementById("counter2");
 var counter2 = new Counter(counter2Elem, 4, 20, 300);
+// setup reset button
 var counter2But = document.getElementById("counter2-but");
-counter2But.addEventListener("click", counter2.reset);
+counter2But.addEventListener("click", counter2.reset.bind(counter2));
+// setup stop button
 var counter2StopBut = document.getElementById("counter2-stop-but");
-counter2StopBut.addEventListener("click", counter2.stop);
+counter2StopBut.addEventListener("click", counter2.stop.bind(counter2));
+// setup start button
 var counter2StartBut = document.getElementById("counter2-start-but");
-counter2StartBut.addEventListener("click", counter2.start);
+counter2StartBut.addEventListener("click", counter2.start.bind(counter2));
+// Start
 counter2.start();
 
 
